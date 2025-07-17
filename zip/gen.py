87ac7e590bcb915b315b4ca298f5a0315c3ee75a
Tim Hatch <tim@timhatch.com>
2025-07-17 08:12:42 -0700

    Zip demos

---
+++ zip/gen.py
@@ -0,0 +1,106 @@
+import mmap
+import zipfile
+import struct
+
+A = b"# benign\n"
+B = b"print('malicious')\n# 12323013111123311000\n"
+
+with zipfile.ZipFile("accept/store.zip", "w") as z:
+    z.writestr("foo", b"abcdefgh", compress_type=0)
+
+with zipfile.ZipFile("accept/deflate.zip", "w") as z:
+    z.writestr("foo", b"abcdefgh", compress_type=8)
+
+with zipfile.ZipFile("accept/subdir.zip", "w") as z:
+    z.mkdir("foo")
+    z.writestr("foo/bar", b"abcdefgh", compress_type=0)
+
+with zipfile.ZipFile("iffy/nosubdir.zip", "w") as z:
+    z.writestr("foo/bar", b"abcdefgh", compress_type=0)
+
+with zipfile.ZipFile("iffy/prefix.zip", "w") as z:
+    z.fp.write(b'    ')
+    z.writestr("foo", b"abcdefgh", compress_type=0)
+
+with zipfile.ZipFile("accept/comment.zip", "w") as z:
+    z.comment = b"hello"
+    z.writestr("foo", b"abcdefgh", compress_type=0)
+
+with zipfile.ZipFile("iffy/extra3byte.zip", "w") as z:
+    zi = zipfile.ZipInfo("foo")
+    zi.extra = b"   "
+    z.writestr(zi, b"abcdefgh", compress_type=0)
+
+with zipfile.ZipFile("iffy/8bitcomment.zip", "w") as z:
+    z.comment = b"\x50\x4b\x05\x06\x00\x00\x00\xff\xff" + b"\x00" * 100
+    z.writestr("foo", b"abcdefgh", compress_type=0)
+
+with open("iffy/suffix_not_comment.zip", "wb") as f:
+    with zipfile.ZipFile(f, "w") as z:
+        z.writestr("foo", b"abcdefgh", compress_type=0)
+    f.write(b'    ')
+
+with open("malicious/short_usize.zip", "w+b") as f:
+    with zipfile.ZipFile(f, "w") as z:
+        z.writestr("file", A+B, compress_type=8)
+
+    data = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_DEFAULT)
+
+    fn1 = data.find(b"file")
+    fn2 = data.find(b"file", fn1+1)
+
+    data[fn1-0x8:fn1-0x8+4] = struct.pack("<L", len(A))
+    data[fn2-0x16:fn2-0x16+4] = struct.pack("<L", len(A))
+
+    # Could also change the crc, but A and A+B have the same
+    # crc = struct.pack("<L", zlib.crc32(b"hello\n"))
+    # data[fn1-0x10:fn1-0x10+4] = crc
+    # data[fn2-0x1e:fn2-0x1e+4] = crc
+
+with open("malicious/short_usize_zip64.zip", "w+b") as f:
+    with zipfile.ZipFile(f, "w", compression=zipfile.ZIP_DEFLATED) as z:
+        with z.open("file", "w", force_zip64=True) as zf:
+            zf.write(A+B)
+        z.filelist[0].file_size = 0xfefefefefefefefe
+
+    data = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_DEFAULT)
+
+    # lfh sizes
+    x1 = data.find(b"\xff\xff\xff\xff\xff\xff\xff\xff")
+    x2 = data.find(b"\xff\xff\xff\xff\xff\xff\xff\xff", x1+8)
+    assert x1 != -1
+    assert x2 != -1
+
+    # 
+    z1 = data.find(b"\x01\x00\x10\x00") + 4
+    z2 = data.find(b"\xfe\xfe\xfe\xfe\xfe\xfe\xfe\xfe")
+
+    (usize, csize) = struct.unpack("<QQ", data[z1:z1+16])
+
+    data[x1:x1+8] = struct.pack("<LL", csize, len(A))
+    data[x2:x2+8] = struct.pack("<LL", csize, len(A))
+    data[z1:z1+16] = struct.pack("<QQ", csize, len(A+B))
+    data[z2:z2+16] = struct.pack("<QQ", csize, len(A+B))
+
+    # data[x1:x1+8] = struct.pack("<L", len("hello\n"))
+    # data[fn2-0x16:fn2-0x16+4] = struct.pack("<L", len("hello\n"))
+
+# with open("malicious/extra_file_stream.zip", "w+b") as f:
+#     with zipfile.ZipFile(f, "w") as z:
+#         z.writestr("foo", b"abcdefgh", compress_type=0)
+#         z.writestr("bar", b"abcdefgh", compress_type=0)
+# 
+#     f.seek(0, 0)
+#     buf = f.read()
+# 
+#     a = buf.find(b"\x50\x4b\x01\02")
+#     b = buf.find(b"\x50\x4b\x01\02", a+4)
+#     c = buf.find(b"\x50\x4b\x05\06", b+4)
+# 
+#     assert b-a == c-b
+# 
+#     d = buf.find(b"\x02\x00\x02\x00")
+# 
+#     buf = buf[:a] + buf[b:c] + buf[a:b] + buf[c:d] + b"\x01\x00\x01\x00" + buf[d+4:-10] + struct.pack("<LLH", c-b, b, 0)
+#     f.seek(0, 0)
+#     f.write(buf)
